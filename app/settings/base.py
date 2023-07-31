e942283e1e2c2d1a13301109a7af988365cbc889
Jeffer-UAO <jeffer443@gmail.com>
2023-07-31 01:21:32 -0500

    css de la API

--- app/settings/base.py
+++ app/settings/base.py
@@ -20,7 +20,7 @@ SECRET_KEY = 'django-insecure-5a-d%)vp9k+0+*^62n)3)dxd0!5w*bwlm!kl-%hve8+d@_a$@x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -131,8 +131,9 @@ TENANT_DOMAIN_MODEL = "customers.Domain"
 
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/uploads/'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -168,7 +169,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -187,4 +188,4 @@ SIMPLE_JWT = {
 #DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
-print(MEDIA_ROOT)
+# print(MEDIA_ROOT)
